3e87f959c26e21afa0ee09e1f3c76c2a2b5716ec
agentVargas2012R <agentVargas2012R@gmail.com>
2016-01-16 14:24:07 -0500

    solution13

---
+++ solution13/app/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('Solutions',['ngRoute']).
+
+controller('MemoCtrl', ['$scope', 'MemoSrv', function($scope, memoSrv){
+	
+	this.memoSrv = memoSrv;
+	this.memos = memoSrv.getMemos();
+
+	this.item = "";
+	this.currentDate = new Date();
+	
+	this.add = function(){
+		memoSrv.addEntry(this.item)
+		this.item = "";
+		memoSrv.notify();
+	};
+}]).
+
+controller('MemoHistoryCtrl', ['$scope', 'MemoSrv', function($scope, memoSrv){
+		var controller = this;
+		controller.lastEntry = memoSrv.lastEntry();
+
+		memoSrv.subscribe($scope, function updateMemos(){
+			controller.lastEntry = memoSrv.lastEntry();
+		});
+}]).
+
+factory('MemoSrv', function($rootScope){
+	return {	
+				//notify both controllers changes can happen.			
+				subscribe: function(scope, callback) {
+					var handler = $rootScope.$on('MemoSrv-Event', callback);
+					scope.$on('$destroy', handler)	
+				},
+				notify: function(){
+					$rootScope.$emit('MemoSrv-Event');	
+				},
+				//internal operations.
+				data: ["Walk the dog", "Do the laundry", "Goto the Grocery Store"],
+				getMemos: function(){
+					return this.data;	
+				}, 
+				addEntry: function(item){
+					this.data.push(item);
+					return this.data;
+				},
+				lastEntry: function(){
+					return this.data[this.data.length - 1]
+				}
+	};
+});
