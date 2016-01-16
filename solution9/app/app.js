539d12743f98d1380ce95293ac9eaaf7df78b9af
agentVargas2012R <agentVargas2012R@gmail.com>
2016-01-16 11:42:18 -0500

    solution9

---
+++ solution9/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('Solutions',['ngRoute']).
+controller('Solution8', [function($scope){
+
+
+	//This defines a variable name on your controller called 'yourName'
+	/**
+	* REFERENCE THIS VARIABLE IN HTML IN THE FOLLOWING WAYS:
+	* 1.) 'controllerName'.yourName --> {{Solutions8.yourName}}
+	* 2.) 'alias'.yourName --> {{soln.yourName}}
+	**/
+	this.yourName = "Mike";
+}]);
