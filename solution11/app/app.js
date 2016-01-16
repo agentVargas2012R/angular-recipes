9327c1f44ba62a827407e4dc8bbb836c4395fb91
agentVargas2012R <agentVargas2012R@gmail.com>
2016-01-16 12:18:34 -0500

    solution11

---
+++ solution11/app/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('Solutions',['ngRoute']).
+controller('Solution8', [function($scope){
+
+
+	//This defines a variable name on your controller called 'yourName'
+	/**
+	* REFERENCE THIS VARIABLE IN HTML IN THE FOLLOWING WAYS:
+	* 1.) 'controllerName'.yourName --> {{Solutions8.yourName}}
+	* 2.) 'alias'.yourName --> {{soln.yourName}}
+	**/
+	this.memos = [];
+	this.item = "";
+
+	//This defines a method name on your controller called 'yourName'
+	/**
+	* REFERENCE THIS METHOD IN HTML IN THE FOLLOWING WAYS:
+	* 1.) 'controllerName'.push() --> {{Solutions8.push()}}
+	* 2.) 'alias'.push() --> {{soln.push()}}
+	**/
+	this.add = function(){
+		this.memos.push(this.item);
+		this.item="";
+	};
+}]);
