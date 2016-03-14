8e5e124d857ba4e039d70f788b6ff4bc8524afa0
Mike Vargas <Mike Vargas>
2016-03-14 09:19:22 -0400

    init

--- exercise13/app/app.js
+++ exercise13/app/app.js
@@ -1,52 +1,16 @@
 'use strict';
 
 angular.module('Solutions',['ngRoute']).
-/* 1.) TODO: INJECT THE COMMON SERVICE INTO THE MemoHIstoryCtrl */
-controller('MemoCtrl', ['$scope', function($scope){
-	
-	this.memoSrv = memoSrv;
-	this.memos = memoSrv.getMemos();
+controller('Solution12', [function($scope){
 
-	this.item = "Default Value";
+
+	//TODO: Initialize your collection with a default entry
+	this.memos = [];
+	this.item = "";
 	this.currentDate = new Date();
-	
+
 	this.add = function(){
-		memoSrv.addEntry(this.item)
-		this.item = "";
-		memoSrv.notify();
+		this.memos.push(this.item);
+		this.item="";
 	};
-}]).
-/* 2.) TODO: INJECT THE COMMON SERVICE INTO THE MemoHIstoryCtrl */
-controller('MemoHistoryCtrl', ['$scope', function($scope){
-		var controller = this;
-		controller.lastEntry = memoSrv.lastEntry();
-
-		memoSrv.subscribe($scope, function updateMemos(){
-			controller.lastEntry = memoSrv.lastEntry();
-		});
-}]).
-
-factory('MemoSrv', function($rootScope){
-	return {	
-				//notify both controllers changes can happen.			
-				subscribe: function(scope, callback) {
-					var handler = $rootScope.$on('MemoSrv-Event', callback);
-					scope.$on('$destroy', handler)	
-				},
-				notify: function(){
-					$rootScope.$emit('MemoSrv-Event');	
-				},
-				//internal operations.
-				data: ["Walk the dog", "Do the laundry", "Goto the Grocery Store"],
-				getMemos: function(){
-					return this.data;	
-				}, 
-				addEntry: function(item){
-					this.data.push(item);
-					return this.data;
-				},
-				lastEntry: function(){
-					return this.data[this.data.length - 1]
-				}
-	};
-});
+}]);
