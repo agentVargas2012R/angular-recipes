7d4cad6a406712a39c23a2b7d5377c03bf5106e3
Mike Vargas <Mike Vargas>
2016-03-14 08:31:18 -0400

    init

---
+++ exercise11/app/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('Solutions',['ngRoute']).
+controller('Solution8', [function($scope){
+
+
+	//This defines a variable name on your controller called 'yourName'
+	/**
+	* REFERENCE THIS VARIABLE IN HTML IN THE FOLLOWING WAYS:
+	* 1.) 'controllerName'.yourName --> {{Solutions8.yourName}}
+	* 2.) 'alias'.yourName --> {{soln.yourName}}
+	**/
+	this.memos = [];
+	this.item = "";
+	this.date = new Date();
+
+	//This defines a method name on your controller called 'yourName'
+	/**
+	* REFERENCE THIS METHOD IN HTML IN THE FOLLOWING WAYS:
+	* 1.) 'controllerName'.push() --> {{Solutions8.push()}}
+	* 2.) 'alias'.push() --> {{soln.push()}}
+	**/
+	
+}]);
