765ff84fb50e1a18f939c121fe0e7f886b042da3
Mike Vargas <Mike Vargas>
2016-03-14 08:26:42 -0400

    init

---
+++ exercise9/app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('Solutions',['ngRoute']).
+controller('Solution8', ['$scope', function($scope){
+
+
+	//This defines a variable name on your controller called 'yourName'
+	/**
+	* REFERENCE THIS VARIABLE IN HTML IN THE FOLLOWING WAYS:
+	* 1.) 'controllerName'.yourName --> {{Solutions8.yourName}}
+	* 2.) 'alias'.yourName --> {{soln.yourName}}
+	**/
+}]);
